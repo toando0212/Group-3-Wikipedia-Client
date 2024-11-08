20e301fd0f1ffc51b61e3c26f39477ed7560456a
toan0212 <toando0212@gmail.com>
2024-11-09 01:28:10 +0700

    cmt

--- app/src/main/java/vn/edu/usth/wikipediaclient/FullArticlelActivity.java
+++ app/src/main/java/vn/edu/usth/wikipediaclient/FullArticlelActivity.java
@@ -2,44 +2,31 @@ package vn.edu.usth.wikipediaclient;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class FullArticlelActivity extends AppCompatActivity {
 
-    private ImageView articleImageView;
     private TextView articleTitle;
     private TextView articleContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_full_article);
 
-
-        //init view
-        articleImageView = findViewById(R.id.articleImage);
+        // Initialize views
         articleTitle = findViewById(R.id.articleTitle);
         articleContent = findViewById(R.id.articleContent);
 
-        //import data from Intent
+        // Import data from Intent
         Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String content = intent.getStringExtra("content");
 
-            String title = intent.getStringExtra("title");
-            String content = intent.getStringExtra("content");
-            int imageResourceId = intent.getIntExtra("image", 0);
-
-            //assgign data to view
-            articleTitle.setText(title);
-            articleContent.setText(content);
-            if (imageResourceId != -1) {
-                articleImageView.setImageResource(imageResourceId);
-            }
-
+        // Assign data to views
+        articleTitle.setText(title);
+        articleContent.setText(content);
     }
 }
