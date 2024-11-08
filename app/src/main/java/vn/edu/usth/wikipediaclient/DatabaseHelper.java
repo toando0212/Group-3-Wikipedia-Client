986519e90585937d64f77a326adfa2ee3e8ec750
Tuong Duy Tuyen <tuyentd.ba12-189@st.usth.edu.vn>
2024-11-09 00:16:11 +0700

    dbhistory

--- app/src/main/java/vn/edu/usth/wikipediaclient/DatabaseHelper.java
+++ app/src/main/java/vn/edu/usth/wikipediaclient/DatabaseHelper.java
@@ -7,95 +7,85 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-
+    // Database Information
     private static final String DATABASE_NAME = "articles.db";
     private static final int DATABASE_VERSION = 1;
 
+    // Table Information
     public static final String TABLE_ARTICLES = "Articles";
+    public static final String COLUMN_ID = "id"; // Primary Key
     public static final String COLUMN_TITLE = "title";
-    public static final String COLUMN_DESCRIPTION = "description";
-    public static final String COLUMN_IMAGE_SRC_ID = "imageSrcId";
+    public static final String COLUMN_DESCRIPTION = "description"; // Optional
+    public static final String COLUMN_IMAGE_SRC_ID = "imageSrcId"; // Image Path/URI
     public static final String COLUMN_CONTENT = "content";
 
+    // Constructor
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // Create Articles Table
         String CREATE_ARTICLES_TABLE = "CREATE TABLE " + TABLE_ARTICLES + " (" +
-                COLUMN_TITLE + " TEXT PRIMARY KEY, " +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TITLE + " TEXT NOT NULL, " +
                 COLUMN_DESCRIPTION + " TEXT, " +
-                COLUMN_IMAGE_SRC_ID + " INTEGER, " +
-                COLUMN_CONTENT + " TEXT" + ")";
+                COLUMN_IMAGE_SRC_ID + " TEXT, " +
+                COLUMN_CONTENT + " TEXT NOT NULL)";
         db.execSQL(CREATE_ARTICLES_TABLE);
-
-        // Insert sample data into the Articles table
-        String INSERT_SAMPLE_DATA = "INSERT INTO " + TABLE_ARTICLES + " (" +
-                COLUMN_TITLE + ", " +
-                COLUMN_DESCRIPTION + ", " +
-                COLUMN_IMAGE_SRC_ID + ", " +
-                COLUMN_CONTENT +
-                ") VALUES " +
-                "('Sample Article 1', 'Description 1', 101, 'Content 1'), " +
-                "('Sample Article 2', 'Description 2', 102, 'Content 2'), " +
-                "('Sample Article 3', 'Description 3', 103, 'Content 3');";
-        db.execSQL(INSERT_SAMPLE_DATA);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if it exists and recreate
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARTICLES);
         onCreate(db);
     }
 
-    // Insert a new article
-    public void addArticle(Article article) {
+    // Insert an Article
+    public long insertArticle(String title, String description, String imageSrcId, String content) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_TITLE, article.getTitle());
-        values.put(COLUMN_DESCRIPTION, article.getDescription());
-        values.put(COLUMN_IMAGE_SRC_ID, article.getImageResourceId());
-        values.put(COLUMN_CONTENT, article.getContent());
+        values.put(COLUMN_TITLE, title);
+        values.put(COLUMN_DESCRIPTION, description);
+        values.put(COLUMN_IMAGE_SRC_ID, imageSrcId);
+        values.put(COLUMN_CONTENT, content);
 
-        long result = db.insert(TABLE_ARTICLES, null, values);
-        db.close();
+        // Insert the row and return the row ID
+        return db.insert(TABLE_ARTICLES, null, values);
     }
 
-    // Retrieve all articles
+    // Get All Articles
     public Cursor getAllArticles() {
         SQLiteDatabase db = this.getReadableDatabase();
-        return db.query(TABLE_ARTICLES, null, null, null, null, null, COLUMN_TITLE + " ASC");
+        return db.query(TABLE_ARTICLES, null, null, null, null, null, COLUMN_ID + " DESC");
     }
 
-    // Update an article by title
-    public int updateArticle(Article article) {
+    // Update an Article
+    public int updateArticle(int id, String title, String description, String imageSrcId, String content) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(COLUMN_DESCRIPTION, article.getDescription());
-        values.put(COLUMN_IMAGE_SRC_ID, article.getImageResourceId());
-        values.put(COLUMN_CONTENT, article.getContent());
+        values.put(COLUMN_TITLE, title);
+        values.put(COLUMN_DESCRIPTION, description);
+        values.put(COLUMN_IMAGE_SRC_ID, imageSrcId);
+        values.put(COLUMN_CONTENT, content);
 
-        int rowsAffected = db.update(TABLE_ARTICLES, values, COLUMN_TITLE + " = ?",
-                new String[]{article.getTitle()});
-        db.close();
-        return rowsAffected;
+        // Update the row with the given ID
+        return db.update(TABLE_ARTICLES, values, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
     }
 
-    // Delete an article by title
-    public void deleteArticle(String title) {
+    // Delete an Article
+    public int deleteArticle(int id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_ARTICLES, COLUMN_TITLE + " = ?", new String[]{title});
-        db.close();
+        return db.delete(TABLE_ARTICLES, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
+    }
+
+    // Search Articles by Title
+    public Cursor searchArticles(String keyword) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_ARTICLES +
+                " WHERE " + COLUMN_TITLE + " LIKE ?";
+        return db.rawQuery(query, new String[]{"%" + keyword + "%"});
     }
 }
-
-// Don't forget to close the cursor when you finish using it to avoid memory leaks.
-// Example usage:
-// Cursor cursor = dbHelper.getAllArticles();
-// if (cursor != null) {
-//     while (cursor.moveToNext()) {
-//         // Process each row
-//     }
-//     cursor.close();
-// }
